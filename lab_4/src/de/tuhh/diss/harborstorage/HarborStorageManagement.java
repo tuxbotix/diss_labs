1e77434e8d17050df5776dfc2faf035e54765537
tuxbotix <darshanaads@gmail.com>
2016-12-26 21:11:11 +0100

    Did Packet, Slot and a bit of HarborStorageManagement

--- lab_4/src/de/tuhh/diss/harborstorage/HarborStorageManagement.java
+++ lab_4/src/de/tuhh/diss/harborstorage/HarborStorageManagement.java
@@ -4,56 +4,60 @@ import de.tuhh.diss.harborstorage.sim.HighBayStorage;
 
 import de.tuhh.diss.harborstorage.sim.StorageException;
 import de.tuhh.diss.harborstorage.sim.PhysicalHarborStorage;
+import de.tuhh.diss.harborstorage.sim.PhysicalCrane;
 
 public class HarborStorageManagement implements HighBayStorage {
-	
+
+	private PhysicalHarborStorage physicalHarborStorage;
 	private PhysicalCrane physicalCrane;
 	private Slot[] slots;
 	private Packet[] packets;
-        private int packetCount;
-	
-	public HarborStorageManagement(){
-            slots = PhysicalHarborStorage.getFirstHarborStorage();
-            physicalCrane =PhysicalHarborStorage.getCrane();
+	private int packetCount;
+
+	public HarborStorageManagement() {
+		physicalHarborStorage = PhysicalHarborStorage.getFirstHarborStorage();
+		physicalCrane = physicalHarborStorage.getCrane();
+		slots = (Slot[]) physicalHarborStorage.getStoragePlacesAsArray();
+		packetCount=0;	
 	}
 
-	public int storePacket(int width, int height, int depth, String description, int weight) throws StorageException {
-		return 0; // TODO: Replace this with your own code. 
-		
+	public int storePacket(int width, int height, int depth,
+			String description, int weight) throws StorageException {
+		return 0; // TODO: Replace this with your own code.
+
 	}
-	
+
 	public void retrievePacket(String description) throws StorageException {
-            for(int i=0; i < 
 		// TODO: Add your own code. 
 	}
-	
+
 	public Packet[] getPackets() {
-		return null; // TODO: Replace this with your own code. 
+		return null; // TODO: Replace this with your own code.
 	}
-	
+
 	public void shutdown() {
-		// TODO: Add your own code. 
+		// TODO: Add your own code.
 	}
-	
-	private Packet getPacketById(int id){
-            Packet packet = null;
-            
-            for(int i=0; i< packets.length ; i++){
-                if(packets[i].getId() == id){
-                    packet = packets[i];
-                }
-            }
-            return packet;
+
+	private Packet getPacketById(int id) {
+		Packet packet = null;
+
+		for (int i = 0; i < packets.length; i++) {
+			if (packets[i].getId() == id) {
+				packet = packets[i];
+			}
+		}
+		return packet;
 	}
-	
-        private Slot getSlotByNumber(int number){
-            Slot slot = null;
-            
-            for(int i=0; i< slots.length ; i++){
-                if(slots[i].getNumber() == number){
-                    slot = slots[i];
-                }
-            }
-            return slot;
+
+	private Slot getSlotByNumber(int number) {
+		Slot slot = null;
+
+		for (int i = 0; i < slots.length; i++) {
+			if (slots[i].getNumber() == number) {
+				slot = slots[i];
+			}
+		}
+		return slot;
 	}
 }
