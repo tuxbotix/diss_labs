9c2a52cca33fd9c81942481016d45a3ec048c627
tuxbotix <darshanaads@gmail.com>
2017-01-26 01:52:03 +0100

    Added Diamond plot and main menu clean up. Need extensive testing.

--- lab_5/src/de/tuhh/diss/plotbot/MainMenu.java
+++ lab_5/src/de/tuhh/diss/plotbot/MainMenu.java
@@ -1,8 +1,6 @@
 package de.tuhh.diss.plotbot;
 
-import de.tuhh.diss.plotbot.geometry.Anchor;
-import de.tuhh.diss.plotbot.geometry.Plottable;
-import de.tuhh.diss.plotbot.geometry.Rectangle;
+import de.tuhh.diss.plotbot.geometry.*;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
@@ -11,9 +9,14 @@ import lejos.util.TextMenu;
 
 public class MainMenu {
 
-	private static final String[] ITEMS = { "Anchor", "Rectangle", "Line" };
+	private static final String[] ITEMS = { "Anchor", "Rectangle", "Diamond",
+			"line" };
 	private static final String TITLE = "Choose Shape to draw:";
 	private TextMenu menu;
+
+	private final int Y_UPPER_BOUND = 230;
+	private final int RECT_X_SHIFT = 20;
+	private final int ANCHOR_X_SHIFT = -20;
 
 	/**
 	 * Creates a new MainMenu object.
@@ -22,6 +25,11 @@ public class MainMenu {
 		menu = new TextMenu(ITEMS, 1, TITLE);
 	}
 
+	/**
+	 * Select a menu item
+	 * 
+	 * @return
+	 */
 	public Plottable select() {
 		int selection = -1;
 		do {
@@ -33,19 +41,33 @@ public class MainMenu {
 		}
 
 		Plottable toDraw = null;
-	
+
 		switch (selection) {
 
 		case 0: {// anchor
-			getIntFromButtons("Width", 3);
-			toDraw = new Anchor();
+			int width = getIntFromButtons("Width", 3);
+			toDraw = new Anchor(width,ANCHOR_X_SHIFT);
+			break;
 		}
 
 		case 1: {
-			getIntFromButtons("Width", 3);
-			toDraw = new Rectangle();			
+			int width = getIntFromButtons("Width", 3);
+			int height = getIntFromButtons("Height", 3);
+			toDraw = new Rectangle(new Coord(RECT_X_SHIFT, Y_UPPER_BOUND
+					- height), new Coord(RECT_X_SHIFT + width, Y_UPPER_BOUND));
 		}
-
+		
+		case 2: {
+			int width = getIntFromButtons("Width", 3);
+			int height = getIntFromButtons("Height", 3);
+			toDraw = new Diamond(new Coord(RECT_X_SHIFT, Y_UPPER_BOUND
+					- height), new Coord(RECT_X_SHIFT + width, Y_UPPER_BOUND));
+		}
+		
+		case 3:{
+			toDraw = new Line(new Coord(-20, 60), new Coord(20, 20));
+		}
+		
 		default: {
 
 		}
@@ -60,6 +82,7 @@ public class MainMenu {
 	 */
 	private int getIntFromButtons(String message, int y) {
 		int number = 0;
+		LCD.clear(y);
 		LCD.drawString(message, 0, y);
 
 		while (!Button.ENTER.isDown()) {
